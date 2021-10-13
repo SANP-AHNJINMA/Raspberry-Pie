daf9b777c10132dce977af18a2fc80e5989f4b18
ispyjun <ispyjun@gmail.com>
2021-10-14 03:41:59 +0900

    raspberry
    raspberry

---
+++ led.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings (False)
+GPIO.setup(17,GPIO.OUT)
+
+while(True):
+    GPIO.output(17,False)
+    time.sleep(16)
+    GPIO.output(17,True)
+    time.sleep(10)
